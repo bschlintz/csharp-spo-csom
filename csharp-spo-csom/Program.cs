00dbe84afce1450fdcf496d363d2181d42dcab0c
Brad Schlintz <brschlin@microsoft.com>
2018-10-18 16:07:23 -0500

    Initial commit

---
+++ csharp-spo-csom/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_spo_csom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://tenant.sharepoint.com";
+            var creds = CredentialManager.GetSharePointOnlineCredential(url);
+
+            using (ClientContext ctx = new ClientContext(url))
+            {
+                ctx.Credentials = creds;
+
+                Web web = ctx.Web;
+                ctx.Load(web, w => w.Title);
+                ctx.ExecuteQuery();
+
+                Console.WriteLine($"Web Title: {web.Title}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
